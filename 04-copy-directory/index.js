a0f4b804a0956bd547f1ce5bc9ae84fbcbd0c212
DrobyshSV <austin@tut.by>
2022-11-07 21:26:53 +0300

    feat: add js code fot 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const fs = require('fs');
+const path = require('path');
 
+const pathFromFolder = path.join(__dirname, 'files');
+const pathToCopyFolder = path.join(__dirname, 'files-copy');
+
+const copyFolder = () => {
+    fs.readdir(__dirname, {withFileTypes: true}, () => {
+        fs.stat(pathToCopyFolder, (error) => {
+            if (error) {
+                fs.mkdir(pathToCopyFolder, () => {
+                    fs.readdir(pathFromFolder, {withFileTypes: true}, (error, files) => {
+                        for (let file of files) {
+                            fs.copyFile(path.join(pathFromFolder, file.name), path.join(pathToCopyFolder, file.name), () => {
+                            })
+                        }
+                    })
+                })
+            } else {
+                fs.readdir(pathToCopyFolder, {withFileTypes: true}, (error, files) => {
+                    for (let file of files) {
+                        fs.unlink(path.join(pathToCopyFolder, file.name), () => {
+                        })
+                    }
+                    fs.readdir(pathFromFolder, {withFileTypes: true}, (error, files) => {
+                        for (let file of files) {
+                            fs.copyFile(path.join(pathFromFolder, file.name), path.join(pathToCopyFolder, file.name), () => {
+                            })
+                        }
+                    })
+                })
+            }
+        })
+    })
+}
+copyFolder()
